9ab46d186eed5f5d175189bc611b43e00ee6ff0c
Emanuel Martinez <emanuel.martinez8@upr.edu>
2023-02-23 01:12:28 -0400

    foto y nombre articulo

--- frontend_model/shopModel.py
+++ frontend_model/shopModel.py
@@ -4,8 +4,8 @@
 # The students must create their own productList when working on their eCommerce site
 # Product images are loaded into static/images/product-images/
 # Done in array instead of dictionaries to portray the differences
-productList = [['1', "Tello Drone", 'DJI', 'desc here', 'Yes', '480p', 'White', 'dji_tello.jpg', '15', 'active', '89', '89'],
-               ['2', 'Bebop 2', 'Parrot', 'desc', 'Yes', '1080p', 'Red', 'parrot_bebop_2.jpg', '3', 'active', '270', '290']]
+productList = [['1', "Bad Bunny Album", 'DJI', 'desc here', 'Yes', '480p', 'White', 'dji_tello.jpg', '15', 'active', '89', '89'],
+               ['2', 'Naruto Shippuden', 'Parrot', 'desc', 'Yes', '1080p', 'Red', 'parrot_bebop_2.jpg', '3', 'active', '270', '290']]
 
 
 def getProductsModel():
