626e8eaf37b19a4d225a9fd15d4a90725bb22d3e
Andre <126026191+AndreProgram@users.noreply.github.com>
2023-02-27 10:13:48 -0400

    restructuracioin de las categorias de los items
    Cambios de front y back en sobre las categorias de los objetos (stickers)

--- frontend_model/shopModel.py
+++ frontend_model/shopModel.py
@@ -4,8 +4,8 @@
 # The students must create their own productList when working on their eCommerce site
 # Product images are loaded into static/images/product-images/
 # Done in array instead of dictionaries to portray the differences
-productList = [['1', "Bad Bunny Album", 'DJI', 'desc here', 'Yes', '480p', 'White', 'dji_tello.jpg', '15', 'active', '89', '89'],
-               ['2', 'Naruto Shippuden', 'Parrot', 'desc', 'Yes', '1080p', 'Red', 'parrot_bebop_2.jpg', '3', 'active', '270', '290']]
+productList = [['1', "Bad Bunny Album", 'Solid', 'desc here', 'Yes', 'Small', 'White', 'dji_tello.jpg', '15', 'active', '89', '89'],
+               ['2', 'Naruto Shippuden', 'Glossy', 'desc', 'Yes', 'Large', 'Red', 'parrot_bebop_2.jpg', '3', 'active', '270', '290']]
 
 
 def getProductsModel():
@@ -14,7 +14,7 @@ def getProductsModel():
 
 def getBrandsModel():
     # Simulating grabbing these filters via SQL from the database
-    brands = ["DJI", "Ruko", "Parrot"]
+    brands = ["Solid", "Glossy", "Metallic"]
     return brands
 
 def getColorsModel():
@@ -23,7 +23,7 @@ def getColorsModel():
 
 
 def getVideoResModel():
-    videores = ["480p", "1080p", "4k"]
+    videores = ["Small", "Medium", "Large"]
     return videores
 
 
