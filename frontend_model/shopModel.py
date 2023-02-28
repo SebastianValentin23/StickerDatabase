e2ac16fa4c74120bdaa74fec5e0c5daed9ab7034
Andre <126026191+AndreProgram@users.noreply.github.com>
2023-02-27 22:06:12 -0400

    The sticker list
    includes images

--- frontend_model/shopModel.py
+++ frontend_model/shopModel.py
@@ -4,8 +4,11 @@
 # The students must create their own productList when working on their eCommerce site
 # Product images are loaded into static/images/product-images/
 # Done in array instead of dictionaries to portray the differences
-productList = [['1', "Bad Bunny Album", 'Solid', 'desc here', 'Yes', 'Small', 'White', 'dji_tello.jpg', '15', 'active', '89', '89'],
-               ['2', 'Naruto Shippuden', 'Glossy', 'desc', 'No', 'Large', 'Red', 'parrot_bebop_2.jpg', '3', 'active', '270', '290']]
+productList = [['1', "Bad Bunny Album", 'Solid', 'desc here', 'Yes', 'Small', 'White', 'bad_corazon_sticker.jpeg', '5', 'active', '89', '89'],
+               ['2', 'Naruto Shippuden', 'Glossy', 'desc', 'No', 'Large', 'Red', 'naruto_sticker.png', '3', 'active', '15', '290'],
+               ['3', 'Chopper One Piece', 'Glossy', 'desc', 'No', 'Medium', 'Red', 'choppersticker.png', '3', 'active', '10', '290'],
+               ['4', 'Bugatti Logo', 'Metallic', 'desc', 'Yes', 'Small', 'Red', 'bugattisticker.png', '3', 'active', '5', '290'],
+               ['5', 'Beach Sunset', 'Glossy', 'desc', 'No', 'Large', 'Red', 'BeachSunsetSticker.png', '3', 'active', '15', '290']]
 
 
 def getProductsModel():
