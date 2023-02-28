726b53afb9bcf438c1e23e9dd8e885723f21f65d
Andre <126026191+AndreProgram@users.noreply.github.com>
2023-02-27 22:25:38 -0400

    fixed images repeating
    Made the images being called for the product to be dinamic with the array
    
     If i could have a super power it would be to never misspell things again

--- frontend_model/shopModel.py
+++ frontend_model/shopModel.py
@@ -4,7 +4,7 @@
 # The students must create their own productList when working on their eCommerce site
 # Product images are loaded into static/images/product-images/
 # Done in array instead of dictionaries to portray the differences
-productList = [['1', "Bad Bunny Album", 'Solid', 'desc here', 'Yes', 'Small', 'White', 'bad_corazon_sticker.jpeg', '5', 'active', '89', '89'],
+productList = [['1', "Bad Bunny Album", 'Solid', 'desc here', 'Yes', 'Small', 'White', 'bad_corazon_sticker.jpg', '5', 'active', '89', '89'],
                ['2', 'Naruto Shippuden', 'Glossy', 'desc', 'No', 'Large', 'Red', 'naruto_sticker.png', '3', 'active', '15', '290'],
                ['3', 'Chopper One Piece', 'Glossy', 'desc', 'No', 'Medium', 'Red', 'choppersticker.png', '3', 'active', '10', '290'],
                ['4', 'Bugatti Logo', 'Metallic', 'desc', 'Yes', 'Small', 'Red', 'bugattisticker.png', '3', 'active', '5', '290'],
