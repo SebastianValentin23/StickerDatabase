812a2efcec14c244359f1b18f32ade06f6540c86
Emanuel Martinez <126026176+EmanuelCCOM@users.noreply.github.com>
2023-03-20 22:42:19 -0400

    Descripcion de los productos
    Edite la descripcion de los productos en el front end. Especificamente en el shop 4 column.

--- frontend_model/shopModel.py
+++ frontend_model/shopModel.py
@@ -4,11 +4,11 @@
 # The students must create their own productList when working on their eCommerce site
 # Product images are loaded into static/images/product-images/
 # Done in array instead of dictionaries to portray the differences
-productList = [['1', "Bad Bunny Album", 'Solid', 'desc here', 'Yes', 'Small', 'White', 'bad_corazon_sticker.jpg', '5', 'active', '5', '5'],
-               ['2', 'Naruto Shippuden', 'Glossy', 'desc', 'No', 'Large', 'Red', 'naruto_sticker.png', '3', 'active', '9', '9'],
-               ['3', 'Chopper One Piece', 'Glossy', 'desc', 'No', 'Medium', 'Red', 'choppersticker.png', '3', 'active', '7', '7'],
-               ['4', 'Bugatti Logo', 'Metallic', 'desc', 'Yes', 'Small', 'Red', 'bugattisticker.png', '3', 'active', '5', '5'],
-               ['5', 'Beach Sunset', 'Glossy', 'desc', 'No', 'Large', 'Red', 'BeachSunsetSticker.png', '3', 'active', '9', '9']]
+productList = [['1', "Bad Bunny Album", 'Solid', 'The iconic heart from Bad Bunnys album art.', 'Yes', 'Small', 'White', 'bad_corazon_sticker.jpg', '5', 'active', '5', '5'],
+               ['2', 'Naruto Shippuden', 'Glossy', 'La vista lateral del ninja mas famoso del anime, Naruto.', 'No', 'Large', 'Red', 'naruto_sticker.png', '3', 'active', '9', '9'],
+               ['3', 'Chopper One Piece', 'Glossy', 'The "wanted" poster of the famous character from the One Piece series, Chopper.', 'No', 'Medium', 'Red', 'choppersticker.png', '3', 'active', '7', '7'],
+               ['4', 'Bugatti Logo', 'Metallic', 'The logo of the luxury car brand Bugatti.', 'Yes', 'Small', 'Red', 'bugattisticker.png', '3', 'active', '5', '5'],
+               ['5', 'Beach Sunset', 'Glossy', 'The beautiful view of a relaxing landscape on the beach.', 'No', 'Large', 'Red', 'BeachSunsetSticker.png', '3', 'active', '9', '9']]
 
 
 def getProductsModel():
